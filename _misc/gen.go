376a937a33b3ebcb7d1fb452d2bfac3f1a613b37
Sato Kazuya <kaz8x131@gmail.com>
2014-02-11 22:18:08 +0900

    フォーク後初のコミット

---
+++ _misc/gen.go
@@ -0,0 +1,45 @@
+// Copyright 2011 Dmitry Chestnykh. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// capgen is an utility to test captcha generation.
+package main
+
+import (
+    "captcha"
+    "flag"
+    "fmt"
+    "log"
+    "os"
+)
+
+var (
+    flagLen  = flag.Int("l", 6, "length of captcha")
+    flagImgW = flag.Int("w", captcha.StdWidth, "image captcha width")
+    flagImgH = flag.Int("h", captcha.StdHeight, "image captcha height")
+)
+
+func usage() {
+    fmt.Fprintf(os.Stderr, "usage: captcha [flags] filename\n")
+    flag.PrintDefaults()
+}
+
+func main() {
+    log.SetFlags(0)
+    flag.Parse()
+    fname := flag.Arg(0)
+    if fname == "" {
+        usage()
+        os.Exit(2)
+    }
+    f, err := os.Create(fname)
+    if err != nil {
+        log.Fatalf("%s", err)
+    }
+    defer f.Close()
+    err = captcha.WriteImage(f,
+        captcha.RandomDigits(*flagLen), *flagImgW, *flagImgH)
+    if err != nil {
+        log.Fatalf("%s", err)
+    }
+}
